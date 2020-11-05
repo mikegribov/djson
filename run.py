e19333ba0d6015aa97b386d80c2a9679e0121fac
mikegribov <mihail.g.gribov@gmail.com>
2020-11-06 01:29:22 +0300

    example countries

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  6 00:14:10 2020
 
-@author: User
-"""
 from filedjson import FiledJson
 
-fj = FiledJson("data")
+fj = FiledJson("examples/countries")
 print(fj)
+
+print (fj.structure)
 
